0d70d05bc702001d9af2b907dfbaa7876ff1bc81
josé victor <josevictorsouza936@gmail.com>
2023-09-21 14:42:15 -0300

    calculadora

---
+++ main.py
@@ -0,0 +1,4 @@
+print ("Calculadora em Python")
+numero1 = int (input("informe um número inteiro"))
+numero2 = int (input("informe outro número inteiro"))
+operacao = input ("Informe a operacao(+ - */")
